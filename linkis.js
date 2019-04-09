a59356655699ee05ae2b58d6200908c8fdbfae4e
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-04-09 15:46:01 -0500

    el el archivo linkis.js utilizando fetch ya pude validar mis links, con la libreria colors puede darle cierto estilo a la hora de imprimir ,solo valida los de 200 , faltan los otros casos

--- linkis.js
+++ linkis.js
@@ -1,13 +1,27 @@
-const fetch = require ('node-fetch');
+const fetch = require('node-fetch');
+const path = require('path');
+ const color =require('colors');
 
 
 const readlink = (data) => {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const arrayLink = data.match(urlRegex);
-    console.log(arrayLink);
-    
- 
-    
-  };
+  const urlRegex = /(https?:\/\/[^\s\)]+)/g;
+  const ObjectLink = data.match(urlRegex);
+  // console.log(ObjectLink);
+if(ObjectLink !== null){
+  ObjectLink.forEach(element => {
+    const route = path.parse(element);
+    // console.log('route', route);
+    const linkName = route.name;
+    fetch(element).then(res => {
+      const linkStatus = res.status
+      //console.log(linkStatus)
+      if (linkStatus ===200){
+        console.log(`${color.bgMagenta(linkName)}\n${color.rainbow(element)}\n ${color.green(linkStatus)} Link Valido\n`)
+      }
+    })
+  })
+}
+};
 
- module.exports.readlink= readlink;
+
+module.exports.readlink = readlink;
--- reader.js
+++ reader.js
@@ -4,18 +4,21 @@ const linkMD = require('./linkis');
 
 const archive = () => {
   fs.readdir('./', (err, data) => {
+    const pathR = path.resolve ('./')
+    console.log ( 'ruta:'+pathR);
     if (err) {
-      console.log('error');
+      console.log('error'+err);
     } else {
       data.forEach(element => {
         if (path.extname(element) === ".md") {
-          fs.readFile(element, 'utf-8', (err, data) => {
+          const doc=  element;
+          fs.readFile(`${pathR}/${doc}`, 'utf-8', (err, data) => {
             if (err){
-              console.log(err);
+              console.log('error'+err);
 
             }else{
             linkMD.readlink(data);
-           
+            
             
             }
           });
